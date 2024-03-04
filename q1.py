f2af06e975ea25d1f03c93d22d4e19e51cd5472d
ArthurWilmer <arthurwrx@gmail.com>
2024-03-04 12:49:18 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,20 @@
-import numpy as np
+import sympy as sp
+
+# Defina a equação
+# equacao = x**4 + 2*x**2 + 1 
+
+
+# Definindo os símbolos
+x = sp.symbols('x')
+
+equacao = x**4 + 2*x**2 + 1
+
+# Resolvendo a equação
+solucoes = sp.solve(equacao, x)
+
+# Verificando e exibindo as soluções
+
+print("Sendo I o número imaginário, As soluções da equação são:")
+for solucao in solucoes:
+    print(solucao)
+
